fc0a31a4c789a619d0850426e5228a0775f06711
lil-fox <developed.by.fox@gmail.com>
2024-01-16 19:24:27 +0100

    Initial commit

---
+++ src/main/java/net/lilfox/betterframes/client/BetterFramesClient.java
@@ -0,0 +1,10 @@
+package net.lilfox.betterframes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterFramesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
